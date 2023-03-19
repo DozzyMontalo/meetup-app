059f26fb8ed47a637207cb942c241020998b5b85
DozzyMontalo <dozienice@gmail.com>
2023-03-19 20:24:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import AllMeetUps from "./pages/AllMeetups";
+import NewMeetUps from "./pages/NewMeetups";
+import Favorites from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<AllMeetUps />} />
+
+        <Route path="/new-meetups" element={<NewMeetUps />} />
+
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </Layout>
   );
-}
+};
 
 export default App;
---
+++ src/store/favorites-context.js
@@ -0,0 +1,44 @@
+import { createContext, useState } from "react";
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+  addFavovorite: (favoriteMeetup) => {},
+  removeFavorite: (meetupId) => {},
+  itemIsFavourite: (meetupId) => {},
+});
+
+export const FavoritesContextProvider = (props) => {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  const addFavoriteHandler = (favoriteMeetup) => {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  };
+
+  const removeFavouriteHandler = (meetupId) => {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter((meetup) => meetup.id !== meetupId);
+    });
+  };
+
+  const itemIsFavouriteHandler = (meetupId) => {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  };
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+    addFavovorite: addFavoriteHandler,
+    removeFavorite: removeFavouriteHandler,
+    itemIsFavourite: itemIsFavouriteHandler,
+  };
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+};
+
+export default FavoritesContext;
